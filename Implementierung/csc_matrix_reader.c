2622fece15ae7ef49596dc7c1f7459f50ad4b42d
Leopold Jofer <projekte@leoj.de>
2024-07-15 12:29:07 +0200

    fix justfile

--- Implementierung/csc_matrix_reader.c
+++ Implementierung/csc_matrix_reader.c
@@ -3,42 +3,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-int readCSCMatrix(const char* filename, csc_matrix* matrix) {
-    // Open file
-    FILE* file = fopen(filename, "r");
-
-    // Check if file was opened successfully
-    if (file == NULL) {
-        fprintf(stderr, "Error opening file\n");
-        return EXIT_FAILURE;
-    }
-
-    // Read matrix dimensions
-    if (fscanf(file, "%d,%d", &matrix->rows, &matrix->cols) != 2) {
-        fprintf(stderr, "Error reading matrix dimensions from file.\n");
-        fclose(file);
-        return EXIT_FAILURE;
-    }
-
-    int res = readFloatLine(file, &matrix->values, &matrix->nnz);
-    if (res < 0) {
-        return res;
-    }
-
-    res = readIntLine(file, &matrix->row_indices, NULL);
-    if (res < 0) {
-        return res;
-    }
-
-    res = readIntLine(file, &matrix->col_ptr, &matrix->col_ptr_length);
-    if (res < 0) {
-        return res;
-    }
-
-    fclose(file);
-    return 0;
-}
+#include <string.h>
 
 int readIntLine(FILE* file, int** vec, size_t* length) {
     const size_t maxLineLength = 100;
@@ -121,4 +86,40 @@ int readFloatLine(FILE* file, float** vec, size_t* length) {
     }
 
     return EXIT_SUCCESS;
+}
+
+int readCSCMatrix(const char* filename, csc_matrix* matrix) {
+    // Open file
+    FILE* file = fopen(filename, "r");
+
+    // Check if file was opened successfully
+    if (file == NULL) {
+        fprintf(stderr, "Error opening file\n");
+        return EXIT_FAILURE;
+    }
+
+    // Read matrix dimensions
+    if (fscanf(file, "%d,%d", &matrix->rows, &matrix->cols) != 2) {
+        fprintf(stderr, "Error reading matrix dimensions from file.\n");
+        fclose(file);
+        return EXIT_FAILURE;
+    }
+
+    int res = readFloatLine(file, &matrix->values, &matrix->nnz);
+    if (res < 0) {
+        return res;
+    }
+
+    res = readIntLine(file, &matrix->row_indices, NULL);
+    if (res < 0) {
+        return res;
+    }
+
+    res = readIntLine(file, &matrix->col_ptr, &matrix->col_ptr_length);
+    if (res < 0) {
+        return res;
+    }
+
+    fclose(file);
+    return 0;
 }
--- justfile
+++ justfile
@@ -1,8 +1,19 @@
+default:
+	@just --list
+
+## C ##
+
+build *FLAGS:
+	cd ./Implementierung && make
+
+run *FLAGS:
+	cd ./Implementierung && make && ./main
+
+
+## Python ##
+
 venv:
 	[ -d venv ] || (python3 -m venv venv && ./venv/bin/pip install -r ./requirements.txt)
 
-build *FLAGS:
-	(cd ./Implementierung && make)
-
-generate *Flags: venv
+generate *FLAGS: venv
 	./venv/bin/python ./Implementierung/generator.py {{FLAGS}}
