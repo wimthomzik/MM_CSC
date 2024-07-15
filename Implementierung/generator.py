a995d3d3377d13eb6c59dff3c3acd876e8877def
Leopold Jofer <projekte@leoj.de>
2024-07-15 12:41:48 +0200

    add save option

--- Implementierung/generator.py
+++ Implementierung/generator.py
@@ -1,9 +1,8 @@
 from argparse import ArgumentParser
 from scipy import sparse
 
-def print_arr(arr: list[int]) -> None:
-    print(",".join(map(str, arr)))
-    
+def print_arr(acc: str, arr: list[int]) -> str:
+    return acc + ",".join(map(str, arr)) + "\n"
 
 def main():
     parser = ArgumentParser()
@@ -11,6 +10,7 @@ def main():
     parser.add_argument('-n', type=int, required=True)
     parser.add_argument('--density', type=float, default=0.01)
     parser.add_argument('--seed', type=int, default=0)
+    parser.add_argument('--save', default=False, action='store_true')
     args = parser.parse_args()
 
 
@@ -20,10 +20,19 @@ def main():
 
     M = sparse.random(m, n, density=density, format='csc', random_state=args.seed)
 
-    print_arr([m,n])
-    print_arr([round(x, 2) for x in M.data])
-    print_arr(M.indices)
-    print_arr(M.indptr)
+    acc = ""
+    acc = print_arr(acc, [m,n])
+    acc = print_arr(acc, [round(x, 2) for x in M.data])
+    acc = print_arr(acc, M.indices)
+    acc = print_arr(acc, M.indptr)
+
+    if args.save:
+        path = f"./Beispiele/{m}-{n}-{density}-{args.seed}.txt"
+        f = open(path, "w")
+        f.write(acc)
+        f.close()
+    else:
+        print(acc, end="")
 
 if __name__ == '__main__':
     main()
